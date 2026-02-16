dedc0a91077cfea873f19b00dc50055135ae021c
lcesbron <lcesbron@student.42lyon.fr>
2026-02-16 11:59:21 +0100

    feat: ex00

--- ex00/include/easyfind.hpp
+++ ex00/include/easyfind.hpp
@@ -1,10 +1,19 @@
 #ifndef EASYFIND_HPP
 # define EASYFIND_HPP
 
-template <typename T>
-int	easyfind(T& container, int toFind)
+template <class T>
+typename T::const_iterator	easyfind(T& container, int toFind)
 {
-	return (*container.std::find(container.std::begin(), container.std::end, toFind));
+	typename T::const_iterator	it = container.begin();
+	typename T::const_iterator	end = container.end();
+
+	while (it < end)
+	{
+		if (*it == toFind)
+			return (it);
+		++it;
+	}
+	return (end);
 }
 
 #endif // EASYFIND_HPP
--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -1,6 +1,14 @@
 #include "easyfind.hpp"
+#include <vector>
+#include <iostream>
 
 int	main(void)
 {
+	std::vector<int>	v;
 
+	for (int i = 0 ; i < 5; i++)
+	{
+		v.push_back(i);
+	}
+	std::cout << *easyfind(v, 7) << std::endl;
 }
