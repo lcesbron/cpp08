efbc360503016e9d9a1170f9b3d4adc0faf534e2
lcesbron <190340561+lcesbron@users.noreply.github.com>
2026-03-02 12:14:52 +0100

    test: ex00 added somes tests

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <iostream>
 
-int	main(void)
+static void	test1(void)
 {
 	std::vector<int>	v;
 
@@ -11,4 +11,21 @@ int	main(void)
 		v.push_back(i);
 	}
 	std::cout << *easyfind(v, 7) << std::endl;
+}
+
+static void	test2(void)
+{
+	std::vector<int>	v;
+
+	for (int i = 0 ; i < 5; i++)
+	{
+		v.push_back(i);
+	}
+	std::cout << *easyfind(v, 4) << std::endl;
+}
+
+int	main(void)
+{
+	test1();
+	test2();
 }
