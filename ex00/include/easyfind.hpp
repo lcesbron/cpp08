79c13b09102484042077a54bcd718c2ab7f2e10f
lcesbron <190340561+lcesbron@users.noreply.github.com>
2026-03-03 13:37:37 +0100

    fix: ex00 use find from stl as subject demands

--- ex00/include/easyfind.hpp
+++ ex00/include/easyfind.hpp
@@ -1,19 +1,12 @@
 #ifndef EASYFIND_HPP
 # define EASYFIND_HPP
 
+# include <algorithm>
+
 template <class T>
 typename T::iterator	easyfind(T& container, int toFind)
 {
-	typename T::iterator	it = container.begin();
-	typename T::iterator	end = container.end();
-
-	while (it < end)
-	{
-		if (*it == toFind)
-			return (it);
-		++it;
-	}
-	return (end);
+	return (find(container.begin(), container.end(), toFind));
 }
 
 #endif // EASYFIND_HPP
